c645b1f6cb9d655bcce92dd0b07ee17f00ec3f5b
Marko Ludlow and sowmya dinavahi <marko@>
2017-04-03 10:03:18 -0700

    add input for destination to places youve been and landmarks for it.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function Places(location,landmark,year){
+  this.mylocation = location;
+  this.landmark= landmark;
+  this.year = year;
+}
+
+Places.prototype.destination = function(){
+  return this.mylocation;
+}
+
+
+$(document).ready(function(){
+  $("form#new-place").submit(function(event){
+    event.preventDefault();
+
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedYear = $("input#new-year").val();
+
+    var myPlaces = new Places(inputtedLocation,inputtedLandmark,inputtedYear);
+
+    $("ul#places").append("<li><span class='places'>" + myPlaces.destination() + "</span></li>");
+    $("input#new-location").val("");
+    $("input#new-landmark").val("");
+    $("input#new-year").val("");
+
+    $("#places").last().click(function(){
+      $("#show-places").show();
+      $("#show-places h2").text(myPlaces.mylocation);
+      $(".location").text(myPlaces.mylocation);
+      $(".landmark").text(myPlaces.landmark);
+      $(".timeofyear").text(myPlaces.year);
+
+  });
+});
+});
